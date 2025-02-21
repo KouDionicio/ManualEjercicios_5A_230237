8b2a5985d446a711327c202518e98fed14fc5649
Citlalli Pérez Dionicio <230237@utxicotepec.edu.mx>
2025-02-21 16:54:31 -0600

    Práctica Angular
    Se arreglo el boton ya que este no tenía estilos

--- ManualEjercicio2_5A_230237/src/main.ts
+++ ManualEjercicio2_5A_230237/src/main.ts
@@ -2,9 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers:[
-    provideAnimations(),
+    provideAnimations(), provideAnimationsAsync(),
   ]
 }).catch((err) => console.error(err));
