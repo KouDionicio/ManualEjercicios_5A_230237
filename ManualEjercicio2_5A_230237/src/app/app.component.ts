f0ac23252860f6f1b27218386d2b875726d29066
Citlalli Pérez Dionicio <230237@utxicotepec.edu.mx>
2025-02-25 10:13:24 -0600

    Práctica Angular
    Se agrego las carpetas de breadcrum, footer, content, page-container

--- ManualEjercicio2_5A_230237/src/app/app.component.ts
+++ ManualEjercicio2_5A_230237/src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from "./components/footer/footer.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { PageContainerComponent } from "./components/page-container/page-container.component";
+import { BreadcrumbComponent } from "./components/breadcrumb/breadcrumb.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, NavbarComponent],
+  imports: [RouterOutlet, MatButtonModule, NavbarComponent, FooterComponent, SidebarComponent, PageContainerComponent, BreadcrumbComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
