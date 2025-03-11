a1ca2ebddeb128051a93251a7bbc4711a5183a19
Citlalli Pérez Dionicio <230237@utxicotepec.edu.mx>
2025-03-10 22:52:45 -0600

    Práctica Angular

--- ManualEjercicio2_5A_230237/src/app/app.component.ts
+++ ManualEjercicio2_5A_230237/src/app/app.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
+import { RouterOutlet } from '@angular/router'; // Para enrutamiento
+import { MatButtonModule } from '@angular/material/button';
+
+// Importar los componentes autónomos
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from "./components/footer/footer.component";
-import { SidebarComponent } from "./components/sidebar/sidebar.component";
-import { PageContainerComponent } from "./components/page-container/page-container.component";
-import { BreadcrumbComponent } from "./components/breadcrumb/breadcrumb.component";
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageContainerComponent } from './components/page-container/page-container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, NavbarComponent, FooterComponent, SidebarComponent, PageContainerComponent, BreadcrumbComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,  // Marca el componente raíz como autónomo
+  imports: [
+    RouterOutlet,  // Necesario para el enrutamiento
+    MatButtonModule,  // Importa otros módulos que necesites
+    NavbarComponent,  // Importa los componentes autónomos
+    SidebarComponent,
+    BreadcrumbComponent,
+    FooterComponent,
+    PageContainerComponent,
+  ]
 })
 export class AppComponent {
   title = 'ManualEjercicio2_5A_230237';
